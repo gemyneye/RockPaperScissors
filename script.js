cdf4d840644329f1872d621b3322e6af070b6c44
Dennis Breitwieser-Cutshall <207044424+gemyneye@users.noreply.github.com>
2025-11-15 14:34:36 -0700

    Updated README.md to include updated pseudocode

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-//Challenge prompt 
-
-//Compare input to randomly selected object
-
+//Challenge prompt for user to select Rock, paper, scissors (case insensitive)
+    // User getHumanChoice function
 //set variables for each object
+
+//Randomly selected object
+    //Computer random getComputerChoice function
+
+//set score variables for user and computer to keep track
+    //humanScore and computerScore each set to 0
+//5 rounds of play (while loop) most wins win
+
+//Function to play round playRound(humanChoice,computerChoice)
+    //Compare objects and declare winner
+    //Display for example (".. wins. {'Rock'} beats {'Scissors'}")
+
 
 //Randomly selected object selected from array?
 
