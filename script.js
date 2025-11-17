0b4e86de6a36e8b9e2d05d873f302dfa838e1bc1
Dennis Breitwieser-Cutshall <207044424+gemyneye@users.noreply.github.com>
2025-11-17 12:16:51 -0700

    Add functions for computer and user selections

--- script.js
+++ script.js
@@ -1,32 +1,49 @@
+//set variables for each object
+//set score variables for user and computer to keep track
+    //humanScore and computerScore each set to 0
+const options = ["rock", "paper", "scissors"];
+let round = 0;
+
 //Challenge prompt for user to select Rock, paper, scissors (case insensitive)
     // User getHumanChoice function
-//set variables for each object
 
-const options = ["rock", "paper", "scissors"];
-let humanScore = 0;
-let computerScore = 0;
-let round = 0;
-function getHumanChoice(){
-    let humanChoice = window.prompt("Please choose 'rock', 'paper', or 'scissors'");
-    console.log(humanChoice);
-}
 
 //Randomly selected object
     //Computer random getComputerChoice function
-function getComputerChoice() {
-    let randomChoice = options[Math.floor(Math.random() * 3)];
-    console.log(randomChoice);
-}
-getComputerChoice();
-getHumanChoice();
-//set score variables for user and computer to keep track
-    //humanScore and computerScore each set to 0
+
+
+
+
 //5 rounds of play (while loop) most wins win
 
 //Function to play round playRound(humanChoice,computerChoice)
     //Compare objects and declare winner
     //Display for example (".. wins. {'Rock'} beats {'Scissors'}")
+let humanScore = 0;
+let computerScore = 0;
 
+function getHumanChoice(){
+        return prompt("Please choose 'rock', 'paper', or 'scissors'");
+}
+
+function getComputerChoice() {
+        return options[Math.floor(Math.random() * 3)];
+}
+// getComputerChoice();
+// getHumanChoice();
+
+function playRound(player, machine) {
+    if (player == machine) {
+        return "tie";
+    } else if (player == "scissors" && machine == "paper") {
+        humanScore += 1;
+        return `You won! Your score: ${humanScore} \n Computer score: ${computerScore}`
+    }
+    console.log(humanScore);
+}
+// console.log(getHumanChoice())
+console.log(getComputerChoice());
+console.log(getHumanChoice());
 
 //Randomly selected object selected from array?
 
