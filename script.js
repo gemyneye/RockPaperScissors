fc680c15c7d0dc8a6beda0f2be0059631be18b8d
Dennis Breitwieser-Cutshall <207044424+gemyneye@users.noreply.github.com>
2025-11-17 06:41:32 -0700

    Add functions for human and computer choices

--- script.js
+++ script.js
@@ -2,9 +2,23 @@
     // User getHumanChoice function
 //set variables for each object
 
+const options = ["rock", "paper", "scissors"];
+let humanScore = 0;
+let computerScore = 0;
+let round = 0;
+function getHumanChoice(){
+    let humanChoice = window.prompt("Please choose 'rock', 'paper', or 'scissors'");
+    console.log(humanChoice);
+}
+
 //Randomly selected object
     //Computer random getComputerChoice function
-
+function getComputerChoice() {
+    let randomChoice = options[Math.floor(Math.random() * 3)];
+    console.log(randomChoice);
+}
+getComputerChoice();
+getHumanChoice();
 //set score variables for user and computer to keep track
     //humanScore and computerScore each set to 0
 //5 rounds of play (while loop) most wins win
